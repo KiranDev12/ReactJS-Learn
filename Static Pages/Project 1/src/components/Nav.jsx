df2d566bb3c8d6fca83d9e6e99fc6af25cb7bf49
KiranDev12 <kirandev.edu@gmail.com>
2023-06-20 10:22:21 +0530

    Project 1 setup - Continue Styling later

---
+++ Static Pages/Project 1/src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import logo from '../assets/logo-white-bg.png'
+
+export default function Nav() {
+    return (
+        <Nav>
+            <img
+                src={logo}
+            />
+        </Nav>
+    )
+}
